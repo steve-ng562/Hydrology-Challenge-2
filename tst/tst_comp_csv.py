b640ea3562ce5e0f95e815c1f1c0afa409a179c4
steve-ng562 <sjng1@uci.edu>
2018-02-19 18:45:10 -0800

    spaces vs tabs ....

--- tst/tst_comp_csv.py
+++ tst/tst_comp_csv.py
@@ -12,5 +12,5 @@ def test():
 	outputData = readCSVFile(outputPath)
 	testData = readCSVFile(testPath)
 	assert outputData.loc[:, "angle_degrees"].tolist() == testData.loc[:, "angle_degrees"].tolist()
-    assert outputData.loc[:, "station_id"].tolist() == testData.loc[:, "station_id"].tolist()
-    assert outPutData.loc[:, "angle_cosine"].tolist() == testData.loc[:, "angle_cosine"].tolist()
+	assert outputData.loc[:, "station_id"].tolist() == testData.loc[:, "station_id"].tolist()
+	assert outPutData.loc[:, "angle_cosine"].tolist() == testData.loc[:, "angle_cosine"].tolist()
